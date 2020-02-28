f5375cef2f4b1b92d17f6251cbf8d48aeeec3326
Salvador <salvador.on@outlook.com>
2020-02-28 17:46:37 -0300

    add rspec for html coding inject

--- spec/write_file_spec.rb
+++ spec/write_file_spec.rb
@@ -2,6 +2,8 @@
 
 require './lib/write_file.rb'
 
+# rubocop: disable Metrics/BlockLength
+
 RSpec.describe Filewrite do
   arr1 = %w[RSPEC-TEST-ruby rails]
   p_name = 'searches/search-related-to-' + "#{arr1.join('-')}.html"
@@ -14,17 +16,36 @@ RSpec.describe Filewrite do
     end
   end
 
+  describe '#build_html' do
+    write = Filewrite.new(arr1)
+    write.build_html
+    write.close_file
+    context 'when build is call write text & file give true' do
+      it { expect(File.foreach(p_name).grep(/html/).any?).to eq(true) }
+    end
+  end
+
   describe '#classify' do
     write = Filewrite.new(arr1)
     write.classify(arr1, arr1)
     write.close_file
-    context 'when classify in call write text file gibe true' do
+    context 'when classify is call write text & file give true' do
       it { expect(File.foreach(p_name).grep(/ruby/).any?).to eq(true) }
+    end
+  end
+
+  describe '#end_html' do
+    write = Filewrite.new(arr1)
+    write.end_html
+    write.close_file
+    context 'when end is call write text & file give true' do
+      it { expect(File.foreach(p_name).grep(/body/).any?).to eq(true) }
     end
   end
 
   describe '#close_file' do
     write = Filewrite.new(arr1)
+    write.build_html
     write.classify(arr1, arr1)
     write.close_file
     context 'when classify in call write text file gibe true' do
@@ -32,3 +53,5 @@ RSpec.describe Filewrite do
     end
   end
 end
+
+# rubocop: enable Metrics/BlockLength
